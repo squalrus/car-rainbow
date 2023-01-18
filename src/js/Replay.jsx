e3035df955924f8305abd20665f19074266261e1
Chad Schulz <chad.awesome@gmail.com>
2023-01-17 21:54:17 -0800

    Optimize for mobile

--- src/js/Replay.jsx
+++ src/js/Replay.jsx
@@ -3,19 +3,12 @@ import React from 'react';
 function Replay(props) {
     return (
         <dialog id="replay" className="dialog">
-            <h3 className="dialog__title">You won! Play again?</h3>
-            <ul className="dialog__options">
-                <li>
-                    <button type="button" className="button" onClick={() => props.replayClick()}>
-                        Yes
-                    </button>
-                </li>
-                <li>
-                    <button type="button" className="button" onClick={() => props.cancelClick()}>
-                        No
-                    </button>
-                </li>
-            </ul>
+            <h3 className="dialog__title">🌈 You did it! 🌈</h3>
+            <div className="dialog__options">
+                <button type="button" className="button" onClick={() => props.replayClick()}>
+                    Play again!
+                </button>
+            </div>
         </dialog>
     );
 }
